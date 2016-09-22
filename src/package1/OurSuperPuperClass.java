15a2235e2b3149233a45a94fa88f00cdc2a80302
Anna Pachkovskaya <Anna Pachkovskaya>
2016-09-22 13:32:40 +0300

    Initial commit

---
+++ src/package1/OurSuperPuperClass.java
@@ -0,0 +1,14 @@
+package package1;
+
+/**
+ * Created by admin on 20.09.2016.
+ */
+//
+public class OurSuperPuperClass {
+
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 5;
+        System.out.println(a+b);
+    }
+}
